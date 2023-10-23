b365148e78e6c09b20568a8dab6705762c526540
Clovis Vinicius Avelar <avelarvnc@gmail.com>
2023-10-23 09:24:21 -0300

    init

---
+++ assets/db/pessoa/insert.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        include_once("../conn.php");
+
+        $nome = $_REQUEST['nome'];
+        $email = $_REQUEST['email'];
+
+        $sql = "INSERT INTO pessoa (nome, email) VALUES ('$nome', '$email')";
+
+        if ($conn->query($sql) == TRUE)
+        {
+            echo "<p>Cadastro realizado.</p>";
+        }
+        else
+        {
+            echo "<p>Erro.</p>";
+        }
+
+        $conn->close();
+
+
+    ?>
+
+    <a href="../../../index.html">Voltar ao início</a>
+    
+</body>
+</html>
+
+
